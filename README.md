965512b0e0b150da060bc4ca9c20e18f47436073
nctu6 <nctu6.tw@gmail.com>
2024-08-01 08:06:19 +0800

    202408 update

--- README.md
+++ README.md
@@ -11,14 +11,14 @@ Script is run monthly, and a monthly `csv`-file outputted to `/data` folder.
 !["Huggingface downloads by model plot."](https://github.com/nctu6/huggingface_stats/blob/main/plots/downloads_by_model.jpg)
 
 ## Table of downloads
-|model_name                          | downloadsAllTime| 2024-06| 2024-07|
-|:-----------------------------------|----------------:|-------:|-------:|
-|Llama3-TAIDE-LX-8B-Chat-Alpha1      |            28615|   10491|   11808|
-|TAIDE-LX-7B-Chat                    |            23488|    5059|   14122|
-|TAIDE-LX-7B                         |             3559|    1454|    1098|
-|Llama3-TAIDE-LX-8B-Chat-Alpha1-4bit |             2099|     671|     412|
-|TAIDE-LX-7B-Chat-4bit               |             1480|     259|     208|
-|b.11.0.0                            |             1096|      36|       5|
-|b.1.0.0                             |              828|       0|      15|
-|a.2.0.0                             |              131|      24|       0|
-|e.1.1.0                             |               64|      14|       0|
+|model_name                          | downloadsAllTime| 2024-06| 2024-07| 2024-08|
+|:-----------------------------------|----------------:|-------:|-------:|-------:|
+|Llama3-TAIDE-LX-8B-Chat-Alpha1      |            55931|   10491|   11808|   27164|
+|TAIDE-LX-7B-Chat                    |            26998|    5059|   14122|    3495|
+|TAIDE-LX-7B                         |             7642|    1454|    1098|    4063|
+|Llama3-TAIDE-LX-8B-Chat-Alpha1-4bit |             2426|     671|     412|     324|
+|TAIDE-LX-7B-Chat-4bit               |             1628|     259|     208|     148|
+|b.11.0.0                            |             1398|      36|       5|     302|
+|b.1.0.0                             |              828|       0|      15|       0|
+|a.2.0.0                             |              131|      24|       0|       0|
+|e.1.1.0                             |               69|      14|       0|       5|
--- plot_stats.R
+++ plot_stats.R
@@ -19,38 +19,16 @@ p_dl_total <- ggplot(data = df_total_sum,
               aes(x = date, y = downloads)) +
   geom_line(colour = "firebrick2") +
   geom_point(shape = 21, size = 1.5, colour = "black", fill = "firebrick2") +
-  expand_limits(y = 0) +
-  scale_y_continuous(breaks = seq(0, max(df_total_sum$downloads) + 5000, by = 20000),
+  theme_light(base_size = 7) +
+  scale_y_continuous(breaks = seq(0, max(df_total_sum$downloads) + 2000, by = 10000),
                      labels = function(x) format(x, big.mark = " ", decimal.mark = ".", scientific = FALSE)) +
   scale_x_date(date_labels = "%Y-%b",
                breaks = unique(df_total_sum$date)[seq(1, length(unique(df_total_sum$date)), by=2)],
                guide = guide_axis(n.dodge = 2)) +
-  theme_light(base_size = 7) +
+  expand_limits(y = 0) +
   labs(y = "Number of downloads",
        x = "Date",
        title = "Total number of downloads per month for TAIDE's models on Huggingface")
-
-
-
-
-# df_sum <- df %>%
-#   group_by(organization, date) %>%
-#   summarize(downloads = sum(downloads))
-#
-# p_dl_org <- ggplot(data = df_sum,
-#                       aes(x = date, y = downloads, fill = organization)) +
-#   geom_line(aes(color = organization)) +
-#   geom_point(shape = 21, size = 3, colour = "black") +
-#   expand_limits(y = 0) +
-#   scale_y_continuous(breaks = seq(0, max(df_sum$downloads) + 3000, by = 10000),
-#                      labels = function(x) format(x, big.mark = " ", decimal.mark = ".", scientific = FALSE)) +
-#   scale_x_date(date_labels = "%b", breaks = unique(df_model_top$date)) +
-#   theme_light(base_size = 8) +
-#   labs(y = "Number of downloads",
-#        x = "Date",
-#        title = "Total number of downloads by organization for TAIDE's models on Huggingface",
-#        fill = "Organization") +
-#   guides(color = "none")
 
 
 df_model <- df %>%
@@ -72,7 +50,7 @@ p_dl_model <- ggplot(data = df_model_top,
   geom_line(aes(color = fct_reorder(model_name, desc(downloads)))) +
   geom_point(shape = 21, size = 1.5, colour = "black") +
   theme_light(base_size = 7) +
-  scale_y_continuous(breaks = seq(0, max(df_model_top$downloads) + 3000, by = 10000),
+  scale_y_continuous(breaks = seq(0, max(df_model_top$downloads) + 2000, by = 10000),
                      labels = function(x) format(x, big.mark = " ", decimal.mark = ".", scientific = FALSE)) +
   scale_x_date(date_labels = "%Y-%b",
                breaks = unique(df_total_sum$date)[seq(1, length(unique(df_total_sum$date)), by=2)],
@@ -91,13 +69,6 @@ ggsave(p_dl_total,
        width = 1920,
        height = 1080,
        units = "px")
-
-# ggsave(p_dl_org,
-#        filename = "plots/downloads_by_org.jpg",
-#        dpi = 300,
-#        width = 1920,
-#        height = 1080,
-#        units = "px")
 
 
 ggsave(p_dl_model,
